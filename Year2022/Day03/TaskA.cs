e1d4923a85a0264ec2093637587ca02dcf8b20d4
Piotr <piotrmalyszek1@gmail.com>
2022-12-03 16:01:19 +0100

    Day03

---
+++ Year2022/Day03/TaskA.cs
@@ -0,0 +1,40 @@
+﻿namespace Year2022.Day03;
+
+public class TaskA
+{
+    public static void Run()
+    {
+        var input = ReadFile();
+        var result = Solve(input);
+        Console.WriteLine(result);
+    }
+
+    private static string Solve(string[] input)
+    {
+        int result = 0;
+        foreach (var line in input)
+        {
+            var first = String.Concat(line.Take(line.Length / 2));
+            var second = String.Concat(line.TakeLast(line.Length / 2));
+            var repetitions = new HashSet<char>();
+            foreach (var letter in first)
+            {
+                if (second.Contains(letter))
+                    repetitions.Add(letter);
+            }
+
+            foreach (var letter in repetitions)
+            {
+                if (Char.IsLower(letter))
+                    result += letter - 96;
+                else
+                    result += letter - 38;
+            }
+        }
+        
+        return result.ToString();
+    }
+
+    private static string[] ReadFile()
+        => File.ReadAllLines(@"E:\Advent-of-Code\Year2022\Day03\input.txt");
+}
---
+++ Year2022/Day03/TaskB.cs
@@ -0,0 +1,41 @@
+﻿namespace Year2022.Day03;
+
+public class TaskB
+{
+    public static void Run()
+    {
+        var input = ReadFile();
+        var result = Solve(input);
+        Console.WriteLine(result);
+    }
+
+    private static string Solve(string[] input)
+    {
+        int result = 0;
+        for (int i = 0; i < input.Length; i+=3)
+        {
+            var first = input[i];
+            var second = input[i + 1];
+            var third = input[i + 2];
+            var repetitions = new HashSet<char>();
+            foreach (var letter in first)
+            {
+                if (second.Contains(letter) && third.Contains(letter))
+                    repetitions.Add(letter);
+            }
+
+            foreach (var letter in repetitions)
+            {
+                if (Char.IsLower(letter))
+                    result += letter - 96;
+                else
+                    result += letter - 38;
+            }
+        }
+        
+        return result.ToString();
+    }
+
+    private static string[] ReadFile()
+        => File.ReadAllLines(@"E:\Advent-of-Code\Year2022\Day03\input.txt");
+}
